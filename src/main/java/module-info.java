974b54d781a7d0dca4d874d52100cfe4ea876457
david <dhuertasl@ucenfotec.ac.cr>
2022-11-27 02:07:41 -0600

    Se crea la interfaz grafica

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.tarea3_ejemplo2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens view to javafx.fxml;
+    exports view;
+}
